ec7a36915b1df2802099ca9ada8368deafa01c64
Hristo Petrov <hpetrovv@gmail.com>
2015-01-10 16:39:57 +0200

    template renaming, working on user ads template

--- Ads/app/js/controllers/user-controller.js
+++ Ads/app/js/controllers/user-controller.js
@@ -1,5 +1,18 @@
-app.controller('UserController', function($scope, $location, pageService,
-	authenticationService, notificationService) {
+app.controller('UserController', function($scope, $location, pageSize, pageService,
+	adsService, authenticationService, notificationService) {
 	pageService.setPageName('My Ads');
 
+	$scope.adsRequestParams = {
+		startPage: 1,
+		pageSize: pageSize
+	}
+
+	$scope.getAds = function(requestParams) {
+		adsService.getUserAds(requestParams)
+			.then(function(data) {
+				$scope.adsData = data;
+			});
+	}
+
+	$scope.getAds($scope.adsRequestParams);
 });
